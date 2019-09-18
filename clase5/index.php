3045a0b35ec19c4a92b9368fa24376bd4ddeddb0
you <you@example.com>
2019-09-17 22:05:50 -0300

    actualizo clase5

--- clase5/index.php
+++ clase5/index.php
@@ -29,17 +29,45 @@ $app->delete('[/]',function (Request $request, Response $response){
 });// Delete: borrar recursos
 */
 
+/**
+ * Retorna objetos
+ */
 $app->get('/datos/',function (Request $request, Response $response){
     $datos= array('nombre'=> ' rogelio', ' apellido'=> 'agua', 'edad'=> 40);
     $newResponse= $response->withJson($datos,200);
     return $newResponse;
 });
 
+/**
+ * Recibe objetos
+ */
 $app->post('/datos/',function (Request $request, Response $response){
-    $ArrayDeParametros = $request->getParseBody();
+    $ArrayDeParametros = $request->getParsedBody();
     $objeto= new stdclass();
-    $objeto->nombre=
-)};
+    $objeto->nombre=$ArrayDeParametros['nombre'];
+    $objeto->apellido=$ArrayDeParametros['apellido'];
+    $newResponse = $response->withJson($objeto,200);
+    return $newResponse;
+});
+
+/**
+ * Recibir objetos y archivos
+ */
+// $app->post('/datos/',function (Request $request, Response $response){
+//     $ArrayDeParametros = $request->getParsedBody();
+//     $objeto= new stdclass();
+//     $objeto->nombre=$ArrayDeParametros['nombre'];
+//     $objeto->apellido=$ArrayDeParametros['apellido'];
+    
+//     $archivos=$request ->getUploadedFiles();
+//     $destino="./fotos/";
+//     $nombreAnterior=$archivos['foto']->getClientFilename();
+//     $extension=explode(".",$nombreAnterior);
+//     $extension=array_reverse($extension);
+//     $archivos['fotos']->moveTo($destino.$objeto->nombre.".".$extension[0]);
+//     $response->getBody()->write("cd");
+//     return $response;
+// });
 
 $app->run();
 
